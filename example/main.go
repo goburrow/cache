3f9036b9bc7f6961f150577a1ca98f9917f5d391
Quoc-Viet Nguyen <afelion@gmail.com>
2016-10-26 00:03:36 +1000

    Add example

--- example/main.go
+++ example/main.go
@@ -1,15 +1,3 @@
-# Cache
-[![GoDoc](https://godoc.org/github.com/goburrow/cache?status.svg)](https://godoc.org/github.com/goburrow/cache) [![Build Status](https://travis-ci.org/goburrow/cache.svg?branch=master)](https://travis-ci.org/goburrow/cache)
-
-Partial implementations of Guava Cache in Go.
-
-## Download
-```
-go get -u github.com/goburrow/cache
-```
-
-## Example
-```go
 package main
 
 import (
@@ -58,4 +46,3 @@ func main() {
 		}
 	}
 }
-```
