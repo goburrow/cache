6baece4bec22bd53ca3208053bbdfd893e1ef4a8
Quoc-Viet Nguyen <afelion@gmail.com>
2016-11-11 09:11:05 +1000

    Hash integers

--- hash.go
+++ hash.go
@@ -18,36 +18,36 @@ func sum(k interface{}) uint64 {
 	}
 	switch h := k.(type) {
 	case int:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case int8:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case int16:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case int32:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case int64:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case uint:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case uint8:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case uint16:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case uint32:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case uint64:
-		return h
+		return hashU64(h)
 	case uintptr:
-		return uint64(h)
+		return hashU64(uint64(h))
 	case float32:
-		return uint64(math.Float32bits(h))
+		return hashU64(uint64(math.Float32bits(h)))
 	case float64:
-		return math.Float64bits(h)
+		return hashU64(math.Float64bits(h))
 	case bool:
 		if h {
-			return 1
+			return hashU64(1)
 		}
-		return 0
+		return hashU64(0)
 	case string:
 		return hashBytes([]byte(h))
 	}
@@ -59,17 +59,28 @@ func sum(k interface{}) uint64 {
 	return 0
 }
 
+const (
+	fnvOffset = 14695981039346656037
+	fnvPrime  = 1099511628211
+)
+
+func hashU64(v uint64) uint64 {
+	// Inline code from hash/fnv to reduce memory allocations
+	var h uint64 = fnvOffset
+	for i := uint(0); i < 8; i++ {
+		h ^= v >> (8 * i) & 0xFF
+		h *= fnvPrime
+	}
+	return h
+}
+
 // hashBytes calculates hash value using FNV-1a algorithm.
 func hashBytes(data []byte) uint64 {
 	// Inline code from hash/fnv to reduce memory allocations
-	const (
-		offset64 = 14695981039346656037
-		prime64  = 1099511628211
-	)
-	var h uint64 = offset64
+	var h uint64 = fnvOffset
 	for _, b := range data {
 		h ^= uint64(b)
-		h *= prime64
+		h *= fnvPrime
 	}
 	return h
 }
@@ -78,7 +89,7 @@ func hashPointer(k interface{}) (uint64, bool) {
 	v := reflect.ValueOf(k)
 	switch v.Kind() {
 	case reflect.Ptr, reflect.UnsafePointer, reflect.Func, reflect.Slice, reflect.Map, reflect.Chan:
-		return uint64(v.Pointer()), true
+		return hashU64(uint64(v.Pointer())), true
 	}
 	return 0, false
 }
--- hash_test.go
+++ hash_test.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"encoding/binary"
 	"hash/fnv"
 	"testing"
 	"unsafe"
@@ -12,32 +13,38 @@ func sumFNV(data []byte) uint64 {
 	return h.Sum64()
 }
 
+func sumFNVu64(v uint64) uint64 {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, v)
+	return sumFNV(b)
+}
+
 func TestSum(t *testing.T) {
 	var tests = []struct {
 		k interface{}
 		h uint64
 	}{
-		{int(-1), ^uint64(1) + 1},
-		{int8(-8), ^uint64(8) + 1},
-		{int16(-16), ^uint64(16) + 1},
-		{int32(-32), ^uint64(32) + 1},
-		{int64(-64), ^uint64(64) + 1},
-		{uint(1), 1},
-		{uint8(8), 8},
-		{uint16(16), 16},
-		{uint32(32), 32},
-		{uint64(64), 64},
-		{byte(255), 255},
-		{rune(1024), 1024},
-		{true, 1},
-		{false, 0},
-		{float32(2.5), 0x40200000},
-		{float64(2.5), 0x4004000000000000},
-		{uintptr(unsafe.Pointer(t)), uint64(uintptr(unsafe.Pointer(t)))},
+		{int(-1), sumFNVu64(^uint64(1) + 1)},
+		{int8(-8), sumFNVu64(^uint64(8) + 1)},
+		{int16(-16), sumFNVu64(^uint64(16) + 1)},
+		{int32(-32), sumFNVu64(^uint64(32) + 1)},
+		{int64(-64), sumFNVu64(^uint64(64) + 1)},
+		{uint(1), sumFNVu64(1)},
+		{uint8(8), sumFNVu64(8)},
+		{uint16(16), sumFNVu64(16)},
+		{uint32(32), sumFNVu64(32)},
+		{uint64(64), sumFNVu64(64)},
+		{byte(255), sumFNVu64(255)},
+		{rune(1024), sumFNVu64(1024)},
+		{true, sumFNVu64(1)},
+		{false, sumFNVu64(0)},
+		{float32(2.5), sumFNVu64(0x40200000)},
+		{float64(2.5), sumFNVu64(0x4004000000000000)},
+		{uintptr(unsafe.Pointer(t)), sumFNVu64(uint64(uintptr(unsafe.Pointer(t))))},
 		{"", 0xcbf29ce484222325},
 		{"string", sumFNV([]byte("string"))},
-		{t, uint64(uintptr(unsafe.Pointer(t)))},
-		{(*testing.T)(nil), 0},
+		{t, sumFNVu64(uint64(uintptr(unsafe.Pointer(t))))},
+		{(*testing.T)(nil), sumFNVu64(0)},
 	}
 
 	for _, tt := range tests {
